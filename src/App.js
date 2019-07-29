daf87c772ee6ec1dedbf742fc8257ffcc6f380cc
jebaquera <jebaquera@gmail.com>
2019-07-29 01:51:40 -0500

    updated colors and shadows.

--- src/App.js
+++ src/App.js
@@ -12,14 +12,15 @@ import popArt from "./popArt.json";
 import "./App.css";
 
 
-// function shufflePopArt(array) {
-//   for (let i = array.length - 1; i > 0; i--) {
-//     let j = Math.floor(Math.random() * (i + 1));
-//     [array[i], array[j]] = [array[j], array[i]];
-//   }
-//   return array;
-// };
+function shufflePopArt(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
 
+// sets state to 0 or empty
 class App extends Component {
   // Set this.state
   state = {
@@ -29,22 +30,45 @@ class App extends Component {
     rightWrong: "",
     clicked: [],
   };
-// sets state to 0 or empty
 
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.handleReset();
+    }
+  };
 
-// when you click on a card ... the pop art image is taken out of the array
+  handleIncrement = () => {
+    const newScore = this.state.currentScore + 1;
+    this.setState({
+      currentScore: newScore,
+      rightWrong: ""
+    });
+    if (newScore >= this.state.topScore) {
+      this.setState({ topScore: newScore });
+    }
+    else if (newScore === 12) {
+      this.setState({ rightWrong: "Amazing!" });
+    }
+    this.handleShuffle();
+  };
 
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      topScore: this.state.topScore,
+      rightWrong: "Pow!",
+      clicked: []
+    });
+    this.handleShuffle();
+  };
 
-// if you click on a pop art image that has already been selected, the game is reset and cards reordered
-
-
-// if you click on an available pop art image, your score is increased and cards reordered
-
-
-// if you get all 12 pop art images correct, you get a congrats message and the game resets        
-
-
-// the order of components to be rendered: nav, title, wrapper, card, footer
+  handleShuffle = () => {
+    let shuffledPopArt = shufflePopArt(popArt);
+    this.setState({ popArt: shuffledPopArt });
+  };
 
 render() {
   return (
@@ -57,7 +81,7 @@ render() {
       />
 
       <Title>
-        This is a memory game. Try to click on each image, but don't click the same picture twice or you lose. Guess all 12 and you win!
+        To play: Click on a pop art masterpiece, but don't click on the same image twice or you lose. Guess all 12 and win!
       </Title>
 
       <Container>
@@ -83,3 +107,17 @@ render() {
 }
 
 export default App;
+
+// when you click on a card ... the pop art image is taken out of the array
+
+
+// if you click on a pop art image that has already been selected, the game is reset and cards reordered
+
+
+// if you click on an available pop art image, your score is increased and cards reordered
+
+
+// if you get all 12 pop art images correct, you get a congrats message and the game resets        
+
+
+// the order of components to be rendered: nav, title, wrapper, card
