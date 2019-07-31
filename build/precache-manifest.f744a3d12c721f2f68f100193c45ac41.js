02830d20f4d38cd8ef56810cb6ece381721c468d
jebaquera <jebaquera@gmail.com>
2019-07-30 21:25:30 -0500

    updated README after publishing.

---
+++ build/precache-manifest.f744a3d12c721f2f68f100193c45ac41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cb4ea0c4e165151b314d947c8fb255b",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "763ed583955349dd7ceb",
+    "url": "/Clicky-Game/static/css/main.d9f09dbc.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "763ed583955349dd7ceb",
+    "url": "/Clicky-Game/static/js/main.58bcbbcb.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
