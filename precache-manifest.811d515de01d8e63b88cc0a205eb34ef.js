811e564f5e28765f8b701aad015efc79da5089c9
jebaquera <jebaquera@gmail.com>
2019-07-31 12:45:47 -0500

    Updates

---
+++ precache-manifest.811d515de01d8e63b88cc0a205eb34ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44039ab83914a4d9e8a5c702e7839897",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "223383c963713d482314",
+    "url": "/Clicky-Game/static/css/main.d9f09dbc.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "223383c963713d482314",
+    "url": "/Clicky-Game/static/js/main.a2bc3239.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
